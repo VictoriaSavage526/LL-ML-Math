5cf78f09e28bad61a49bc7feaa20c71cf5b26c2b
VictoriaSavage526 <vsavage526@gmail.com>
2020-03-24 22:19:04 -0400

    attempting to add basic file

---
+++ module01.py
@@ -0,0 +1,6 @@
+print("Here begins the first module in python format for the LinkedInLearning Course \"Math for Machine Learning\"")
+print("This is the transciption to the best of my ability from the Jupyter Notebooks: ")
+
+
+# Module 01-02 - Linear Equations, Module 01-01 is merely boolean checking
+
